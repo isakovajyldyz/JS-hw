c8a309e6eb30971e1fa6e2822c06a2f05fff9517
isakovajyldyz <jisakova806@gmail.com>
2023-02-04 15:59:55 +0600

    1less

---
+++ 1les/main.js
@@ -0,0 +1,96 @@
+alert(123)
+
+//готовые функции в JS
+
+//alert()
+
+//prompt('how old are you?', 23)
+
+//confirm('Вы хотите покинуть сайт?')
+
+//переменные
+
+//var let const
+
+//let age = prompt('how old are u?')
+//alert(age)
+
+//let age = confirm('how old are u?')
+//alert(age)
+
+//let a=5
+//console. log(a)
+//console.error(a)
+
+//let a100=5
+// $ _ -можно использовать в переменных
+// нельзя в переменных использовать такие слова let cons function и т д
+
+//let firstName = 5 or first_name
+//firstName=10
+
+//let a = 5 - can be chanched
+//const b = 10- can't be changed
+
+//типы данных - 8
+//number string bigInt boolean null undefined symbol object
+//не учим bigInt symbol
+//примитивные данные number string null undefined  boolean
+//object
+
+//let a=100; //100.1 -100 Infinity Nan-not a number
+//let b = '100';//"" '' ``
+//let c = true or false
+// null
+//undefined
+
+//let person = {
+//name:"Sanjar",
+//age :18,
+//car:true}
+//console.log(person)
+
+//let a = 100
+//console.log(typeof a)
+//console.log(typeof (a))
+
+
+
+//let a = 100
+//console.log(typeof a+100) - number 100
+//console.log(typeof (a+100)) - number
+
+//let a = 100
+//console.log(typeof typeof a) - string
+
+//преобразование типов
+//console.log(number(value:true)) - 1
+//console.log(number(value:'100')) - 100
+//console.log(number(value:'100a')) - NaN
+//console.log(number(value:null)) - 0
+//console.log(number(value:undefined)) - NaN
+//console.log(number(value:{})) - NaN
+
+//console.log(-true)
+//console.log(true*1)
+//console.log(true/1)
+//console.log(parseInt(string:'100a')) - 100
+//console.log(parseInt(string:'a100a')) - NaN
+//console.log(parseInt(string:'100.5a')) - 100
+//console.log(parseFloat(string:'100.5a')) - 100.5
+
+///console.log(string(value:1))
+///console.log(string(value:true))
+///console.log(string(value:null))
+///console.log(string(value:undefined))
+///console.log(string(value:{}))
+
+console.log(1+'') -конкатенация
+console.log(true+'') - конкатенация
+
+
+
+
+
+
+
