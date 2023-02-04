94af222f4e33029df90e9ad7f6ce5eaf81e0c341
isakovajyldyz <jisakova806@gmail.com>
2023-02-04 18:06:24 +0600

    1hw

---
+++ 1hw/main.js
@@ -0,0 +1,35 @@
+let a = 10
+console.log(a)
+
+let test = "hello world"
+console.log(typeof test)
+
+let b = 12
+b = 20
+console.log(typeof b)
+
+let n = 30
+alert(n)
+
+let str = 'hello world'
+console.log(Number('hello world'))
+
+
+let c =123
+console.log(String('123'))
+console.log(typeof c)
+
+let d = true
+console.log(true*1)
+
+let x = null
+console.log(Number(null))
+
+let y = undefined
+console.log(Number( undefined))
+
+let f = false
+console.log(Number( false))
+
+
+
